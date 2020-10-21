89d6302e6938e0ec6eae1319ed4ff8fbdeb2f2c1
rhalatiu <fotogramemtrie92>
2020-10-21 15:32:55 +0300

    First Commit

---
+++ src/main/java/school/siit/Student.java
@@ -0,0 +1,72 @@
+package school.siit;
+
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String ID;
+
+    public Student(String firstName, String lastName, String dateOfBirth, String gender, String ID) {
+        this.dateOfBirth = dateOfBirth;
+
+        if (!firstName.equals("")) {
+            this.firstName = firstName;
+        }else throw new IllegalArgumentException("First name should not be empty");
+
+
+        if (!lastName.equals("")) {
+            this.lastName = lastName;
+        }else throw new IllegalArgumentException("Last name should not be empty");
+
+
+
+        if ((!gender.toLowerCase().equals("male")) && (!gender.toLowerCase().equals("female")) || (!gender.toLowerCase().equals("m")) && (!gender.toLowerCase().equals("f"))) {
+            throw new IllegalArgumentException("The gender should be male(m) or female(f)");
+        } else this.gender = gender;
+
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    /*public void addStudent() throws Exception{
+        Scanner firstNameScanner = new Scanner(System.in);
+        System.out.println("Add the student's first name");
+        try {
+            String firstNameValue = firstNameScanner.nextLine();
+            System.out.println(firstNameValue);
+        }catch (Exception e){
+        System.out.println("No correct value");}
+
+        Scanner lastNameScanner = new Scanner(System.in);
+        System.out.println("Add the student's last name");
+        try{
+            String lastNameValue = lastNameScanner.nextLine();
+            System.out.println(lastNameValue);
+        }catch (Exception e){
+            System.out.println("Incorrect value added");
+        }
+    }*/
+
+}
---
+++ src/main/java/school/siit/StudentRepository.java
@@ -0,0 +1,5 @@
+package school.siit;
+
+public class StudentRepository {
+
+}
